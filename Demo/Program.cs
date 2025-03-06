0fbe52bfedb24444cbc956d0047297520400fdfe
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-07 01:54:25 +0200

    Part 01 Intro To Delegate &Example 01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Test Demo Push");
-        }
-    }
+			#region Part 01 Intro To Delegate &Example 01
+			//int Result = StringFunctions.GetCountOfUpperChars();
+
+			//Step 1 : Declare ref from delegate
+			StringFuncDelegate Ref;
+
+			//Step 2 : initialize ref from delegate [Point to Function]
+			//Ref = new StringFuncDelegate(StringFunctions.GetCountOfUpperChars);
+			Ref = StringFunctions.GetCountOfUpperChars; //sentex Suger
+														//Ref += StringFunctions.GetCountOfLowerChars;
+														//Step 3: Use Delegate Reference [Call Method]
+			int Result = Ref.Invoke("IsLAm");
+			Console.WriteLine(Result);
+			#endregion
+		}
+	}
 }
---
+++ Demo/StringFunctions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	// 0 : Delegate Declaration
+	public delegate int StringFuncDelegate(string s);
+	//1-new Delegate (Class), the ref from this Delegate can refere to Function or more than function [Pointer to function]
+	//2-these function may be class member function or object member function
+	//3-these function must be the same signature of the delegate
+	//4-reraldless function access modifer or name of the function
+
+	internal class StringFunctions
+	{
+		public static int GetCountOfUpperChars(string Name)
+		{
+			int count = 0;
+			if (Name is not null)
+			{
+				for (int i = 0; i < Name.Length; i++)
+				{
+					if (char.IsUpper(Name[i]))
+						count++;
+				}
+			}
+			return count;
+		}
+
+		public static int GetCountOfLowerChars(string Name)
+		{
+			int count = 0;
+			if (Name is not null)
+			{
+				for (int i = 0; i < Name.Length; i++)
+				{
+					if (char.IsLower(Name[i]))
+						count++;
+				}
+			}
+			return count;
+		}
+	}
+}
