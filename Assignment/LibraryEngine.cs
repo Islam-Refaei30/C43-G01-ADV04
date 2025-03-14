6e736b30987d7b23d50970e3947c5032416ec9ab
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-14 03:45:55 +0200

    All Assignment

---
+++ Assignment/LibraryEngine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+	public delegate T FPTR<T>(Book B);
+	internal class LibraryEngine
+	{
+		public static void ProcessBooks<T> (List<Book> BList, FPTR<T> ftr)
+		{
+			foreach (Book book in BList)
+			{
+				Console.WriteLine(ftr(book));
+			}
+		}
+
+		public static void ProcessBooksBuiltInDelegate<T>(List<Book> BList, Func<Book, T> func)
+		{
+			foreach (Book book in BList)
+			{
+				Console.WriteLine(func(book));
+			}
+		}
+	}
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,6 +5,42 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Test Assignment Push");
+
+
+			List<Book> books = new List<Book>
+			{
+			new Book("978-3-16-148410-0", "C# Basics", new string[] { "John Doe", "Jane Doe" }, new DateTime(2021, 5, 10), 29.99m),
+			new Book("978-1-23-456789-7", "Advanced C#", new string[] { "Mark Smith" }, new DateTime(2020, 8, 15), 49.99m),
+			new Book("978-0-12-345678-9", "Design Patterns", new string[] { "Erich Gamma", "Richard Helm" }, new DateTime(2019, 3, 22), 59.99m)
+			};
+
+			#region With use define delegate
+			//// Title
+			//Console.WriteLine("\nBook Titles:");
+			//LibraryEngine.ProcessBooks(books, BookFunctions.GetTitle);
+
+			//// Authors
+			//Console.WriteLine("\nBook Authors:");
+			//LibraryEngine.ProcessBooks(books, BookFunctions.GetAuthors);
+
+			//// Price
+			//Console.WriteLine("\nBook Prices:");
+			//LibraryEngine.ProcessBooks(books, BookFunctions.GetPrice); 
+			#endregion
+
+			#region With Built in delegate
+			// Title
+			Console.WriteLine("\nBook Titles:");
+			LibraryEngine.ProcessBooksBuiltInDelegate(books, BookFunctions.GetTitle);
+
+			// Authors
+			Console.WriteLine("\nBook Authors:");
+			LibraryEngine.ProcessBooksBuiltInDelegate(books, BookFunctions.GetAuthors);
+
+			// Price
+			Console.WriteLine("\nBook Prices:");
+			LibraryEngine.ProcessBooksBuiltInDelegate(books, BookFunctions.GetPrice);
+			#endregion
 		}
 	}
 }
